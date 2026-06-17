e887ccf29a4c2d71f006433e1dc5720db0a75d79
UBSITBrettBagleo <20246485@s.ubaguio.edu>
2026-06-17 08:59:06 +0800

    Fixed app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
 
+import { Home } from './pages/home/home';
+import { Catalog } from './pages/catalog/catalog';
+import { Cart } from './pages/cart/cart';
+
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'catalog', component: CatalogComponent },
-    { path: 'cart', component: CartComponent }
-  ];
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'catalog',
+    component: Catalog
+  },
+  {
+    path: 'cart',
+    component: Cart
+  }
+];
